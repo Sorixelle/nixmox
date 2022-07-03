883506b1cac02cbb269543e6890cb62f2b8d652d
Ruby Iris Juric <ruby@srxl.me>
2022-07-03 23:51:59 +1000

    Fix build failure if plugin has no python deps

--- packages/oomox/default.nix
+++ packages/oomox/default.nix
@@ -19,7 +19,11 @@ let
 in
 if builtins.length plugins == 0 then basePkg else
 (basePkg.override {
-  extraPythonDeps = ps: builtins.concatMap (p: p.pythonDeps ps) plugins;
+  extraPythonDeps =
+    let
+      pluginsWithDeps = builtins.filter (p: p ? pythonDeps) plugins;
+    in
+    ps: builtins.concatMap (p: p.pythonDeps ps) pluginsWithDeps;
 }).overrideAttrs (old: {
   name = "${old.pname}-${old.version}-with-plugins";
 

aacc8651cdb914c4ce776d224953254a658aa918
Ruby Iris Juric <ruby@srxl.me>
2022-07-05 18:35:49 +1000

    Remove redundant oomox subdir of packages

--- overlay.nix
+++ overlay.nix
@@ -1,12 +1,12 @@
 final: prev:
 
 {
-  oomox = prev.callPackage ./packages/oomox {
-    unwrapped = prev.callPackage ./packages/oomox/base.nix { };
+  oomox = prev.callPackage ./packages/default.nix {
+    unwrapped = prev.callPackage ./packages/base.nix { };
     withPlugins = plugins: final.oomox.override { inherit plugins; };
   };
 
   oomoxFull = final.oomox.withPlugins (builtins.attrValues final.oomoxPlugins);
 
-  oomoxPlugins = import ./packages/oomox/plugins { pkgs = final; };
+  oomoxPlugins = import ./packages/plugins { pkgs = final; };
 }
